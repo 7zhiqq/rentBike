0d8c316b5e7b4c4dc3bfa32334f3ff48757cd58f
Kheishia Faith <kfgmationg@gmail.com>
2025-04-12 10:19:03 +0800

    Final Project

--- main/website/models.py
+++ main/website/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# class Record(models.Model):
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     first_name = models.CharField(max_length=50)
-#     last_name =  models.CharField(max_length=50)
-#     email = models.CharField(max_length=100)
-#     phone = models.CharField(max_length=12)
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name =  models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
     
-#     def __str__(self):
-#         return (f"{self.first_name} {self.last_name}")
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name}")
